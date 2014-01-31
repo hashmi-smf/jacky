3d0026940c26a548a705ea969a89a6db2eef11f4
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2014-01-31 13:35:10 +0530

    Set default url options

--- lib/jacky/railtie.rb
+++ lib/jacky/railtie.rb
@@ -4,6 +4,14 @@ module Jacky
       #preload these
       Jacky::Git.version
       Jacky.settings
+
+      #set the default url options
+      options = Jacky.config.default_url_options
+      if options
+        config.action_mailer.default_url_options = options
+        Rails.application.routes.default_url_options = options
+      end
+
     end
   end
 end
